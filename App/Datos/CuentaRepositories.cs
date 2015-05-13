550349b09455a16a6256daad5583308d47cc333a
alfonsopate <alfonscor@gmail.com>
2015-05-13 16:57:58 -0500

    mejorar

---
+++ App/Datos/CuentaRepositories.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using App.Modelo;
+using System.Xml;
+using System.Web.Hosting;
+using System.IO;
+using System.Xml.Linq;
+
+
+namespace App.Datos
+{
+    public class PersonasRepositories
+    {
+        private static List<Cuentas> data = new List<Cuentas>();
+
+        public List<Cuentas> getCuentas()
+        {
+            // Creamos la lista genérica de Personas
+            List<Cuentas> lista = new List<Cuentas>();
+            // Obtenemos la ruta de archivo XML
+            string ruta = HttpContext.Current.Server.MapPath("/Datos/Cuentas.xml");
+
+            XDocument doc = XDocument.Load(ruta);
+
+            var people = from p in doc.Descendants("Cuentas") select p;
+
+            foreach (XElement p in people.Elements("Cuentas"))
+            {
+                Cuentas person = new Cuentas(
+                                                  p.Element("ID").Value,
+                                                  p.Element("Nombres").Value,
+                                                  p.Element("Apellidos").Value,
+                                                  p.Element("EmailP").Value,
+                                                  p.Element("EmailW").Value,
+                                                  p.Element("TelefonoP").Value,
+                                                  p.Element("TelefonoW").Value
+
+                                              );
+                lista.Add(person);
+
+            }
+
+
+            return lista;
+        }
+
+        public void add(Cuentas p)
+        {
+            data.Add(p);
+            WriteXML(data);
+
+        }
+
+        private void WriteXML(List<Cuentas> list)
+        {
+            XmlTextWriter xmlwriter = new XmlTextWriter(HttpContext.Current.Server.MapPath("/Datos/Agenda.xml"), System.Text.Encoding.UTF8);
+
+            //Inicio XML Documento
+            xmlwriter.WriteStartDocument(true);
+            xmlwriter.Formatting = Formatting.Indented;
+            xmlwriter.Indentation = 2;
+
+            //ROOT Element
+            xmlwriter.WriteStartElement("Cuentas");
+            //Call create nodes method
+            foreach (Cuentas p in list)
+            {
+                xmlwriter.WriteStartElement("Persona");
+                xmlwriter.WriteElementString("ID", p.Id);
+                xmlwriter.WriteElementString("Nombres", p.Nombres);
+                xmlwriter.WriteElementString("Apellidos", p.Apellidos);
+                xmlwriter.WriteElementString("EmailP", p.EmailP);
+                xmlwriter.WriteElementString("EmailW", p.EmailW);
+                xmlwriter.WriteElementString("TelefonoP", p.TelefonoP);
+                xmlwriter.WriteElementString("TelefonoW", p.TelefonoW);
+                xmlwriter.WriteEndElement();
+            }
+
+            xmlwriter.WriteEndElement();
+
+            //End XML Document
+            xmlwriter.WriteEndDocument();
+
+            //Close Write
+            xmlwriter.Close();
+        }
+
+    }
+}
---
+++ App/Web/Listar.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Web/Site1.Master" AutoEventWireup="true" CodeBehind="Listar.aspx.cs" Inherits="App.Web.Listar" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <form id="form1" runat="server">
+    <div class="jumbotron">
+        <h2>Listar Cuentas</h2>
+        <div class ="table-responsive">
+            <asp:GridView ID="GridView1" runat="server" AllowPaging="True" AutoGenerateColumns="False" DataSourceID="ObjectDataSource1" CssClass="table table-responsive table-bordered table-condensed">
+
+                <Columns>
+                    <asp:BoundField DataField="Id" HeaderText="Id" SortExpression="Id" />
+                    <asp:BoundField DataField="Nombres" HeaderText="Nombres" SortExpression="Nombres" />
+                    <asp:BoundField DataField="Apellidos" HeaderText="Apellidos" SortExpression="Apellidos" />
+                    <asp:BoundField DataField="EmailP" HeaderText="EmailP" SortExpression="EmailP" />
+                    <asp:BoundField DataField="EmailW" HeaderText="EmailW" SortExpression="EmailW" />
+                    <asp:BoundField DataField="TelefonoP" HeaderText="TelefonoP" SortExpression="TelefonoP" />
+                    <asp:BoundField DataField="TelefonoW" HeaderText="TelefonoW" SortExpression="TelefonoW" />
+                </Columns>
+            </asp:GridView>
+            <asp:ObjectDataSource ID="ObjectDataSource1" runat="server" SelectMethod="getPersonas" TypeName="App.Datos.PersonasRepositories"></asp:ObjectDataSource>
+    </div>
+
+    </div>
+
+</form>
+
+</asp:Content>
